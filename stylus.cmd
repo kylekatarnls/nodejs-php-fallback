6e593a5e4cd32c9d8da9ae474c860429aa76064a
kylekatarnls <kylekatarnls@gmail.com>
2016-07-24 16:27:51 +0200

    Composer plugin (#5)

--- stylus.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\stylus\bin\stylus" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\stylus\bin\stylus" %*
-)
