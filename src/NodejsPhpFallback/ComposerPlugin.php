87d0c7ffb2dadfdb64f2f51d0d4a7cfbc1529d19
Kyle <kylekatarnls@gmail.com>
2016-07-24 15:06:53 +0200

    Experiment with composer plugin

---
+++ src/NodejsPhpFallback/ComposerPlugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NodejsPhpFallback;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class ComposerPlugin implements PluginInterface
+{
+    // public function activate(Composer $composer, IOInterface $io)
+    // {
+    //     $installer = new TemplateInstaller($io, $composer);
+    //     $composer->getInstallationManager()->addInstaller($installer);
+    // }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'post-autoload-dump' => "NodejsPhpFallback\\NodejsPhpFallback::install",
+        );
+    }
+}
