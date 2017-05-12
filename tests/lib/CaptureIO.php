af1628ceef46a80a4d0cbc67d1b4341ef3a0b06a
kylekatarnls <kylekatarnls@gmail.com>
2017-05-12 12:16:01 +0200

    Add unit tests

--- tests/lib/CaptureIO.php
+++ tests/lib/CaptureIO.php
@@ -5,10 +5,23 @@ use Composer\IO\NullIO;
 class CaptureIO extends NullIO
 {
     protected $lastMsg;
+    protected $errored;
 
     public function write($msg, $newline = true, $verbosity = self::NORMAL)
     {
         $this->lastMsg = $msg;
+        $this->errored = false;
+    }
+
+    public function writeError($msg, $newline = true, $verbosity = self::NORMAL)
+    {
+        $this->lastMsg = $msg;
+        $this->errored = true;
+    }
+
+    public function isErrored()
+    {
+        return $this->errored;
     }
 
     public function getLastOutput()
